d308e5a63ede001229d968b616d637b2cb61a851
s111ew <167631312+s111ew@users.noreply.github.com>
2025-01-15 12:59:10 +0000

    Reorganise file structure

--- app/frontend/src/components/Grid.jsx
+++ app/frontend/src/components/Grid.jsx
@@ -1,4 +1,38 @@
-import shirts from "../shirts"
+import shirt1 from '../assets/images/shirt-one.jpg'
+import shirt2 from '../assets/images/shirt-two.jpg'
+import shirt3 from '../assets/images/shirt-three.png'
+import shirt4 from '../assets/images/shirt-four.png'
+
+const shirts = [
+  {
+    id: "1",
+    imageUrl: shirt1,
+    brand: "misc",
+    color: "off-white",
+    year: "2024",
+  },
+  {
+    id: "2",
+    imageUrl: shirt2,
+    brand: "queen",
+    color: "off-white",
+    year: "1983",
+  },
+  {
+    id: "3",
+    imageUrl: shirt3,
+    brand: "rolling stones",
+    color: "black",
+    year: "2020",
+  },
+  {
+    id: "4",
+    imageUrl: shirt4,
+    brand: "misc",
+    color: "black",
+    year: "2021",
+  },
+];
 
 function Grid() {
   const cells = shirts.map((shirt, index) => {
